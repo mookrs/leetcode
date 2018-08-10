81eea011144395500a1155a57e33baec9c8402fb
Mookrs <mookrs@gmail.com>
2018-08-10 21:24:14 +0800

    Add Problem 1, 2

---
+++ 1_two_sum.py
@@ -0,0 +1,54 @@
+class Solution1:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+
+s1 = Solution1()
+print(s1.twoSum([2, 7, 11, 15], 9))
+
+
+class Solution2:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for idx, num in enumerate(nums):
+            nums_dict[num] = idx
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in nums_dict and nums_dict[complement] != idx:
+                return [idx, nums_dict[complement]]
+
+
+s2 = Solution2()
+print(s2.twoSum([2, 7, 11, 15], 9))
+
+
+class Solution3:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in nums_dict:
+                return [nums_dict[complement], idx]
+            nums_dict[num] = idx
+
+
+s3 = Solution3()
+print(s3.twoSum([2, 7, 11, 15], 9))
---
+++ 2_add_two_numbers.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        p, q = l1, l2
+        curr = dummy
+        carry = 0
+        while p is not None or q is not None:
+            x = p.val if p is not None else 0
+            y = q.val if q is not None else 0
+            sum = x + y + carry
+            carry = sum // 10
+            curr.next = ListNode(sum % 10)
+            curr = curr.next
+            if p is not None:
+                p = p.next
+            if q is not None:
+                q = q.next
+
+        if carry == 1:
+            curr.next = ListNode(carry)
+
+        return dummy.next
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+s = Solution()
+result = s.addTwoNumbers(l1, l2)
+curr = result
+while curr is not None:
+    print(curr.val)
+    curr = curr.next
